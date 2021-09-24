c5976eb9d1a9083d7abbf74c49c0c33b6d959c30
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2021-09-24 16:22:08 -0400

    stronger recommendations for teams with big leads

--- bot/bot_functions.R
+++ bot/bot_functions.R
@@ -7,11 +7,11 @@ tweet_play <- function(df) {
     nfl4th::add_4th_probs() %>%
     nfl4th::make_table_data() %>%
     arrange(-choice_prob)
-  
+
   play_desc <- df$desc %>%
     stringr::str_replace("\\([:digit:]*\\:[:digit:]+\\)\\s", "") %>%
     substr(1, 100)
-  
+
   choice_emoji <- dplyr::case_when(
     # football to punt
     df$type_text %in% c("Blocked Punt", "Punt") ~ "\U0001f3c8\U0001f9B5",
@@ -23,21 +23,23 @@ tweet_play <- function(df) {
     df$type_text %in% c("Penalty") ~ "\U0001f6A8",
     TRUE ~ ""
   )
-  
+
+  # wp1 = preferred choice
+  # wp2 = second choice
   wp1 <- tableData %>% dplyr::slice(1) %>% pull(choice_prob)
   wp2 <- tableData %>% dplyr::slice(2) %>% pull(choice_prob)
-  
+
   diff <- wp1 - wp2
   choice <- tableData %>% dplyr::slice(1) %>% pull(choice)
   choice <- if_else(abs(diff) < 1, "Toss-up", choice)
-  
+
   rec_emoji <- dplyr::case_when(
     choice == "Go for it" ~ "\U0001f449",
     choice == "Field goal attempt" ~ "\U0001f45F\U0001f3c8",
     choice == "Punt" ~ "\U0001f3c8\U0001f9B5",
     choice == "Toss-up" ~ "\U0001f937"
   )
-  
+
   confidence <- case_when(
     abs(diff) < 1 ~ "",
     abs(diff) >= 1 & abs(diff) < 3 ~ "(MEDIUM)",
@@ -45,43 +47,43 @@ tweet_play <- function(df) {
     abs(diff) >= 5 & abs(diff) <= 10 ~ "(VERY STRONG)",
     abs(diff) > 10 ~ "(YOU BETTER DO THIS)"
   )
-  
+
   confidence <- if_else(
-    confidence == "(MEDIUM)" & abs(wp1 / wp2) > 1.2,
+    confidence == "(MEDIUM)" & (abs(wp1 / wp2) > 1.2 | (100 - wp2) / (100 - wp1) > 1.2),
     "(STRONG)",
     confidence
   )
-  
+
   position <- if_else(
-    !is.na(df$yardline), 
+    !is.na(df$yardline),
     glue::glue("at the {df$yardline}"),
     glue::glue("{df$yardline_100} yards from opponent end zone")
   )
-  
+
   posteam <- df$posteam
   defteam <- if_else(df$posteam == df$home_team, df$away_team, df$home_team)
-  
+
   table <- make_table(tableData, df)
-  
+
   table %>% gtsave("bot/post.png")
-  
-  text <- 
+
+  text <-
     glue::glue(
       "
   ---> {df$away_team} ({df$away_score}) @ {df$home_team} ({df$home_score}) <---
   {posteam} has 4th & {df$ydstogo} {position}
-               
+
   Recommendation {confidence}: {rec_emoji} {choice} (+{round(diff, 1)} WP)
   Actual play: {choice_emoji} {play_desc}
   ")
-  
-  
+
+
   # don't post if every choice is < 1 or > 99
   if (wp1 > 1 & wp2 > 1 & wp1 < 99 & wp2 < 99) {
     post_tweet(text, media = "bot/post.png")
   }
   # post_tweet(text)
-  
+
 }
 
 
