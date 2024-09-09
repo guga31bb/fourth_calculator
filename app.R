ed9b5c24ed9fce9f5b7c5f54e52bfeb3899061fd
Ben B <1425357+guga31bb@users.noreply.github.com>
2024-09-09 08:58:37 -0400

    turn on 2024

--- app.R
+++ app.R
@@ -43,7 +43,7 @@ ui <- function(request) {
                                    selectInput(
                                      inputId =  "season",
                                      label = "Season:",
-                                     choices = 2014:2023,
+                                     choices = 2014:2024,
                                      selected = 2020
                                    ),
 
